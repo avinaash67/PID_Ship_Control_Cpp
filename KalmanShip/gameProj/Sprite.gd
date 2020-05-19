19f9bcf433854cc38381e9048b47d7d1e7f40d2b
avinaash67 <avinaash67manoharan@gmail.com>
2020-05-19 23:33:55 +0200

    Kalman ship updated 2.0

--- KalmanShip/gameProj/Sprite.gd
+++ KalmanShip/gameProj/Sprite.gd
@@ -10,6 +10,9 @@ var angle = 0
 const sensorNoise_l = -2.0
 const sensorNoise_h = 2.0
 
+var prev_t = 0
+var delta_t = 0
+
 var commClient
 var commServer
 
@@ -19,8 +22,8 @@ func _ready():
 	curPos = get_offset()
 	print(curPos)
 	commClient = PacketPeerUDP.new()
-	#commClient.set_dest_address("127.0.0.1",4243)
-	commClient.set_dest_address("169.254.49.190",4243)
+	commClient.set_dest_address("127.0.0.1",4243)
+#	commClient.set_dest_address("169.254.49.190",4243)
 	
 	commServer = PacketPeerUDP.new()
 	#commServer.set_dest_address("127.0.0.1",4244)
@@ -51,30 +54,37 @@ func _process(delta):
 	receiveCorrection()
 	
 	#Setting the new position obtained
-	newPos.x = curPos.x + speed + kalman.x
-	newPos.y = curPos.y + kalman.y
-	var slope = (newPos.y-curPos.y)/(newPos.x-curPos.x)
-	angle = tan(slope)
-	rotate(deg2rad(angle))
-	print(newPos)
-	set_offset(newPos)
+	newPos.x = curPos.x - 10 + kalman.x
+	newPos.y = curPos.y + 20 + kalman.y
 	
+#	var slope = (newPos.y-curPos.y)/(newPos.x-curPos.x)
+#	angle = tan(slope)
+#	rotate(deg2rad(angle))
+#	print(newPos)
+
+	#Measuring system clock ticks
+	var curr_t = OS.get_ticks_msec()
+	delta_t = curr_t - prev_t
+	prev_t = curr_t
+#	print(curr_t)
+	
+	set_offset(newPos)
+
 	#Sensor noise addition
 	#var my_random_number = rng.randf_range(-10.0, 10.0)
 	#Sending measurement 
-	sendMeasurement()
+	
+	sendMeasurement(curr_t)
 
 func plot():
 	pass
 	
-func sendMeasurement():
-	#Measuring system clock ticks
-	OS.get_ticks_usec()
-	
+func sendMeasurement(var curr_t):
 	var separator = ":"
 	var text = "$MEAS"+ separator + String(newPos.x + sensorNoise()) + separator + \
-				String(newPos.y + sensorNoise()) + separator + String(angle)
-				
+				String(newPos.y + sensorNoise()) + separator +  String(delta_t) + \
+				separator + String(curr_t)
+	print(text)
 	commClient.put_packet(text.to_ascii())
 	
 	
